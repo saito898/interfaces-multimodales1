0316799dee99e1b2ca5c0dfd0b12e5f8ec377adf
saito898 <saloaran1909@gmail.com>
2026-08-20 13:36:12 -0500

    Implement two-column layout with subheaders
    Added two columns with subheaders and content.

--- app.py
+++ app.py
@@ -11,3 +11,15 @@ st.write ("somos los novios mas lindos del mundo")
 texto= st.text_input("aaaaaah!!!!","este es mi texto")
 st.write("el texto escrito es", texto)
 
+st.subheader("ahora usemos 2 columnas")
+
+col1,col2= st.columns(2)
+with col1:
+  st.subheader("esta es la primera columna")
+  st.write("las interfaces multimodales mejoran la experiencia de usuario")
+  resp=st.checkbox("estoy de acuerdo")
+  if resp
+  st.write("correcto")
+
+with col2:
+  st.subheader("esta es la segunda columna")
